3562245892b984d3365afc838b53848572523b09
João Davi <joaodaviaraujo2006@gmail.com>
2025-11-29 15:58:12 -0300

    Atualiza o projeto com novas funcionalidades

--- Projeto/src/main.c
+++ Projeto/src/main.c
@@ -2,21 +2,51 @@
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
+#include <string.h>
 
-#define LIN 5 // número de linhas
-#define COL 5 // número de colunas
-#define MINAS 5 // número de minas
+#define MINAS 5   // número de minas fixo no jogo
 
-// inicializa o campo com 0 e posiciona as minas (-1)
-void inicializaCampo(int campo[LIN][COL]) {
-    for (int i = 0; i < LIN; i++)
-        for (int j = 0; j < COL; j++)
+// FUNÇÕES DE ALOCAÇÃO
+
+// aloca dinamicamente uma matriz int ** (lin x col)
+int **alocaMatriz(int lin, int col) {
+    int **m = malloc(lin * sizeof(int *));
+    if (m == NULL) {
+        printf("Erro: memória insuficiente.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < lin; i++) {
+        m[i] = malloc(col * sizeof(int));
+        if (m[i] == NULL) {
+            printf("Erro: memória insuficiente.\n");
+            exit(1);
+        }
+    }
+
+    return m;
+}
+
+// libera matriz alocada
+void liberaMatriz(int **m, int lin) {
+    for (int i = 0; i < lin; i++)
+        free(m[i]);
+    free(m);
+}
+
+// FUNÇÕES DO JOGO
+
+// inicializa o campo com 0 e distribui minas (-1)
+void inicializaCampo(int **campo, int lin, int col) {
+    for (int i = 0; i < lin; i++)
+        for (int j = 0; j < col; j++)
             campo[i][j] = 0;
 
     int minas_colocadas = 0;
     while (minas_colocadas < MINAS) {
-        int i = rand() % LIN;
-        int j = rand() % COL;
+        int i = rand() % lin;
+        int j = rand() % col;
+
         if (campo[i][j] != -1) {
             campo[i][j] = -1;
             minas_colocadas++;
@@ -24,90 +54,142 @@ void inicializaCampo(int campo[LIN][COL]) {
     }
 }
 
-// imprime o campo para o jogador
-void imprimeCampo(int campo[LIN][COL], int revelado[LIN][COL]) {
-    printf("Campo:\n");
-    for (int i = 0; i < LIN; i++) {
-        for (int j = 0; j < COL; j++) {
-            if (revelado[i][j]) {
-                if (campo[i][j] == -1)
-                    printf("* ");
-                else
-                    printf("%d ", campo[i][j]);
-            } else
-                printf("- ");
-        }
-        printf("\n");
-    }
-}
-
 // conta minas adjacentes
-int minasAdjacentes(int campo[LIN][COL], int linha, int col) {
+int minasAdjacentes(int **campo, int lin, int col, int i, int j) {
     int cont = 0;
-    for (int i = linha - 1; i <= linha + 1; i++) {
-        for (int j = col - 1; j <= col + 1; j++) {
-            if (i >= 0 && i < LIN && j >= 0 && j < COL && !(i == linha && j == col))
-                if (campo[i][j] == -1)
+
+    for (int x = i - 1; x <= i + 1; x++) {
+        for (int y = j - 1; y <= j + 1; y++) {
+            if (x >= 0 && x < lin && y >= 0 && y < col && !(x == i && y == j))
+                if (campo[x][y] == -1)
                     cont++;
         }
     }
+
     return cont;
 }
 
-int main() {
-    setlocale(LC_ALL, "Portuguese"); // para ajuste da acentuação nos printfs
-    int campo[LIN][COL];
-    int revelado[LIN][COL] = {0};
-    int linha, col;
-    int jogadas_restantes = LIN * COL - MINAS;
-    int ganhou = 0;
+// imprime o campo
+void imprimeCampo(int **campo, int **revelado, int **flag, int lin, int col) {
+    printf("\n   ");
+    for (int j = 0; j < col; j++)
+        printf("%d ", j + 1);
 
+    printf("\n");
+
+    for (int i = 0; i < lin; i++) {
+        printf("%d  ", i + 1);
+
+        for (int j = 0; j < col; j++) {
+            if (flag[i][j]) {
+                printf("F ");   // F = flag
+            }
+            else if (!revelado[i][j]) {
+                printf("- ");  // escondido
+            }
+            else if (campo[i][j] == -1) {
+                printf("* ");  // mina
+            }
+            else {
+                printf("%d ", campo[i][j]); // número
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// FUNÇÃO PRINCIPAL
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
     srand(time(NULL));
 
-    inicializaCampo(campo);
+    int lin = 5, col = 5;
+    int **campo = alocaMatriz(lin, col);
+    int **revelado = alocaMatriz(lin, col);
+    int **flag = alocaMatriz(lin, col);
 
-    printf("Bem-vindo ao Campo Minado!\n");
-    printf("Escolha linhas e colunas de 1 a %d para revelar.\n", LIN);
+    // zera revelado e flag
+    for (int i = 0; i < lin; i++)
+        for (int j = 0; j < col; j++) {
+            revelado[i][j] = 0;
+            flag[i][j] = 0;
+        }
 
-    while (!ganhou) {
-        imprimeCampo(campo, revelado);
+    inicializaCampo(campo, lin, col);
 
-        printf("Digite a linha e a coluna que deseja revelar separadas por espaço (1 a %d): ", LIN);
-        scanf("%d %d", &linha, &col);
+    char nome[50];
+    printf("Digite seu nome: ");
+    fgets(nome, 50, stdin);
+    nome[strcspn(nome, "\n")] = '\0';  // remove '\n'
 
-        // Ajusta para índice do vetor (0 a 4)
-        linha--;
-        col--;
+    printf("Bem-vindo, %s, ao Campo Minado!\n", nome);
 
-        if (linha < 0 || linha >= LIN || col < 0 || col >= COL) {
-            printf("Posição inválida! Tente novamente.\n");
+    int jogadas_restantes = lin * col - MINAS;
+    int linha, coluna;
+    char comando;
+
+    while (1) {
+        imprimeCampo(campo, revelado, flag, lin, col);
+
+        printf("Comandos:\n");
+        printf("  r L C  -> revelar\n");
+        printf("  f L C  -> marcar/desmarcar bandeira\n");
+        printf("Ex: r 2 3\n");
+
+        printf("Digite comando: ");
+        scanf(" %c %d %d", &comando, &linha, &coluna);
+
+        linha--; coluna--;
+
+        if (linha < 0 || linha >= lin || coluna < 0 || coluna >= col) {
+            printf("Posição inválida!\n");
             continue;
         }
 
-        if (revelado[linha][col]) {
-            printf("Posição já revelada! Tente outra.\n");
+        if (comando == 'f') {  // marca bandeira
+            if (!revelado[linha][coluna]) {
+                flag[linha][coluna] = !flag[linha][coluna];
+                printf("Bandeira %s!\n", flag[linha][coluna] ? "colocada" : "removida");
+            }
             continue;
         }
 
-        if (campo[linha][col] == -1) {
-            printf("BOOM! Você acertou uma mina. Fim de jogo.\n");
-            // revela todas as minas
-            for (int i = 0; i < LIN; i++)
-                for (int j = 0; j < COL; j++)
-                    if (campo[i][j] == -1) revelado[i][j] = 1;
-            imprimeCampo(campo, revelado);
-            break;
-        } else {
-            campo[linha][col] = minasAdjacentes(campo, linha, col);
-            revelado[linha][col] = 1;
+        if (comando == 'r') {  // revelar
+            if (flag[linha][coluna]) {
+                printf("Posição marcada como bandeira. Remova antes de revelar.\n");
+                continue;
+            }
+
+            if (campo[linha][coluna] == -1) {
+                printf("BOOM! Você acertou uma mina. Fim de jogo.\n");
+
+                // revela todas as minas
+                for (int i = 0; i < lin; i++)
+                    for (int j = 0; j < col; j++)
+                        if (campo[i][j] == -1)
+                            revelado[i][j] = 1;
+
+                imprimeCampo(campo, revelado, flag, lin, col);
+                break;
+            }
+
+            revelado[linha][coluna] = 1;
+            campo[linha][coluna] = minasAdjacentes(campo, lin, col, linha, coluna);
             jogadas_restantes--;
+
             if (jogadas_restantes == 0) {
-                printf("Parabéns! Você venceu!\n");
-                ganhou = 1;
-                imprimeCampo(campo, revelado);
+                printf("Parabéns, %s! Você venceu!\n", nome);
+                imprimeCampo(campo, revelado, flag, lin, col);
+                break;
             }
         }
     }
+
+    liberaMatriz(campo, lin);
+    liberaMatriz(revelado, lin);
+    liberaMatriz(flag, lin);
 
     return 0;
 }
